f492d9da7b689148b0b682f0507427c8e7088011
HeRku <herrikurniawan211@gmail.com>
2025-06-26 03:06:54 +0800

    numberOne done

--- numberOne.dart
+++ numberOne.dart
@@ -6,5 +6,19 @@
   Jelaskan mengapa Anda memilih tipe data tertentu untuk setiap variabel.
 */
 void main(List<String> args) {
-  
+  // alasan penggunaan tipe data Map untuk manajemen data karena mengakses data lebih cepat,
+  // serta mencegah adanya duplikasi data, serta memudahkan dan memanipulasi data tersebut
+  var product = {
+    'Buku': {'jumlah': 10, 'harga': 10.000},
+    'Pensil': {'jumlah': 5, 'harga': 2.000},
+    'Pena': {'jumlah': 7, 'harga': 3.000},
+  };
+  print(product);
+  // Manipulasi data sistem manajemen inventaris
+  // Mengambil data Buku dengan jumlah
+  print(product['Buku']!['jumlah']);
+  // Mengambil data Pensil dengan harga
+  print(product['Pensil']!['harga']);
+  // Menambahkan data pada variabel product
+  product['Penghapus'] = {'jumlah': 12, 'harga': 1.000};
 }
