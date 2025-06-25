2ce4f511dfb4b70de9ac33281e6e34ed5776f67a
HeRku <herrikurniawan211@gmail.com>
2025-06-26 00:27:07 +0800

    add exercise numberOne

---
+++ numberOne.dart
@@ -0,0 +1,10 @@
+/*
+  Manajemen Inventaris Sederhana: Sebuah toko kecil ingin membuat sistem manajemen inventaris sederhana. 
+  Mereka perlu menyimpan nama produk (String), jumlah stok (int), dan harga per unit (double). 
+  Buatlah program Dart yang mendeklarasikan variabel untuk tiga produk berbeda dengan detail tersebut, 
+  lalu cetak informasi lengkap untuk setiap produk ke konsol. 
+  Jelaskan mengapa Anda memilih tipe data tertentu untuk setiap variabel.
+*/
+void main(List<String> args) {
+  
+}
