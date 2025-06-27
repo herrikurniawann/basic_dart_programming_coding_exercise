bd30102f759ac8693f5d356e0e0cc3a171a768f8
HeRku <herrikurniawan211@gmail.com>
2025-06-27 14:39:07 +0800

    numberThree done

--- numberThree.dart
+++ numberThree.dart
@@ -6,3 +6,34 @@
   Demonstrasikan cara menambahkan mata kuliah baru ke List dan menambahkan nilai untuk mata kuliah tersebut ke Map. 
   Jelaskan juga bagaimana Anda akan mencari nilai mata kuliah tertentu menggunakan Map.
 */
+
+void main(List<String> args) {
+  // Menggunakan nested MAP
+  var mataKuliah = {
+    1.0: {
+      'algoritma pemrograman': {'nama': 'John doe', 'nilai': 80},
+      'pemrograman web': {'nama': 'John doe', 'nilai': 85},
+      'struktur data': {'nama': 'John doe', 'nilai': 87},
+    },
+    2.0: {
+      'algoritma pemrograman': {'nama': 'Stevan', 'nilai': 90},
+      'pemrograman web': {'nama': 'Stevan', 'nilai': 88},
+      'struktur data': {'nama': 'Stevan', 'nilai': 80},
+    },
+  };
+  // print(mataKuliah);
+  // untuk mengambil nama kata kuliah tertentu nama dan nilainya
+  print(mataKuliah[1.0]?['algoritma pemrograman']?['nama']);
+  print(mataKuliah[1.0]?['algoritma pemrograman']?['nilai']);
+
+  print(mataKuliah[2.0]?['algoritma pemrograman']?['nama']);
+  print(mataKuliah[2.0]?['algoritma pemrograman']?['nilai']);
+
+  // untuk menambahkan data sebagai berikut:
+  mataKuliah[1.0]?['matematika diskrit'] = {'nama': 'john doe', 'nilai': 79};
+  print(mataKuliah);
+
+  // untuk mengapus data sebagai berikut:
+  mataKuliah[1.0]?.remove('matematika diskrit');
+  print(mataKuliah[1.0]);
+}
