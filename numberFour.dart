1ee285f1f61c0b41abd8a2dc4c4aad4e908c3cbd
HeRku <herrikurniawan211@gmail.com>
2025-06-27 14:48:54 +0800

    nummberFour done

--- numberFour.dart
+++ numberFour.dart
@@ -4,3 +4,11 @@
   Buatlah sebuah Set di Dart untuk menyimpan ID pengguna unik dari kumpulan data awal yang berisi duplikasi. 
   Setelah itu, tambahkan beberapa ID pengguna baru ke Set tersebut dan cetak semua ID unik yang tersimpan.
 */
+
+void main(List<String> args) {
+  var numberId = [01, 01, 02, 03, 02, 04, 05, 06, 07, 08, 09, 10, 11];
+  // konversi ke Set untuk mencegah duplikasi data
+  var _numberId = {...numberId};
+  _numberId.add(11);
+  print(_numberId);
+}
